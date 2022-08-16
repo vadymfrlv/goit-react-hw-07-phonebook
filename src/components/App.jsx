d1aa3fd0ac0593e67138c4cb2f0db5c56acc0ab5
Vadym Frolov <vdfroloff@gmail.com>
2022-08-16 23:35:09 +0300

    upds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Section from './Section';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 import Notification from './Notification';
-// import { getVisibleContacts } from 'redux/contacts/contacts-selectors';
-import { getContacts } from 'redux/contacts/contacts-operations';
+import { getVisibleContacts } from 'redux/contacts/contacts-selectors';
+// import { getContacts } from 'redux/contacts/contacts-operations';
 import styles from './Filter/Filter.module.css';
 
 export default function App() {
-  // const contacts = useSelector(getVisibleContacts);
-  const { contacts } = getContacts();
+  const contacts = useSelector(getVisibleContacts);
+  // const contacts = getContacts();
 
   return (
     <div
@@ -24,7 +24,7 @@ export default function App() {
       </Section>
 
       <Section title="Contacts">
-        {contacts && contacts.length > 0 ? (
+        {contacts.length > 0 ? (
           <>
             <div className={styles.filter}>All contacts: {contacts.length}</div>
             <Filter />
