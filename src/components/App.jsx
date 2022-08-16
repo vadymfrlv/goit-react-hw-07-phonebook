bedda52d1cb7eeceef2278a6b8054ca4bcd57f82
Vadym Frolov <vdfroloff@gmail.com>
2022-08-16 23:19:27 +0300

    upds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import Section from './Section';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 import Notification from './Notification';
-import { getVisibleContacts } from 'redux/contacts/contacts-selectors';
+// import { getVisibleContacts } from 'redux/contacts/contacts-selectors';
 import { getContacts } from 'redux/contacts/contacts-operations';
 import styles from './Filter/Filter.module.css';
 
 export default function App() {
-  const contacts = useSelector(getVisibleContacts);
-  // const { contacts } = getContacts();
+  // const contacts = useSelector(getVisibleContacts);
+  const { contacts } = getContacts();
 
   return (
     <div
